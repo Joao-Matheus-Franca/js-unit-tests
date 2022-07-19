1b91f9c949f074242248132a15e099932bd27408
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-19 16:53:41 -0300

    Retornando ao formato original do describe

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -14,7 +14,7 @@ const numbers = require('../src/numbers');
 
 */
 
-describe('Casos de teste para a função `numbers`', () => {
+describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
     expect(numbers([1, 2, 3, 4, 5])).toBe(true);
     expect(numbers([1, 2, '3', 4, 5])).toBe(false);
