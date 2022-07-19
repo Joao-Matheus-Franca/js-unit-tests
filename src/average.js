dea23824dc30f378581b0d264d869b2c72ddfb8a
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-19 15:23:10 -0300

    Concluido: requisito 01

--- src/average.js
+++ src/average.js
@@ -11,6 +11,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+   return undefined;
+  }; 
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] != "number") {
+      return undefined;
+    } else {
+    soma = soma + array[i];
+    }
+  }
+  let media = Math.round(soma/array.length);
+  return media;
+};
 
 module.exports = average;
