be6b6de9088952f67032b8fbb45fc678a88462a3
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-19 15:38:33 -0300

    Ajustando erros do lint no requisito 01

--- src/average.js
+++ src/average.js
@@ -15,15 +15,14 @@ const average = (array) => {
   let soma = 0;
   if (array.length === 0) {
    return undefined;
-  }; 
+  } 
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof array[i] != "number") {
+    if (typeof array[i] !== 'number') {
       return undefined;
-    } else {
-    soma = soma + array[i];
     }
+    soma += array[i];
   }
-  let media = Math.round(soma/array.length);
+  let media = Math.round(soma / array.length);
   return media;
 };
 
